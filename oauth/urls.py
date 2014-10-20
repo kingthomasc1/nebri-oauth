67affa01dea2e4b38f5a98df57d378e0bec64f99
craktok <king.thomas.c@gmail.com>
2014-10-20 10:15:56 -0700

    added sender

--- oauth/urls.py
+++ oauth/urls.py
@@ -4,7 +4,8 @@ urlpatterns = patterns('',
     url(r'^smartthings/oauth/authorize/$',
         'smartthings.views.oauth_authorize',
         name='Smart Things authorize'),
-    url(r'^smartthings/oauth/callback/(?P<client_id>[^/]+)/(?P<client_secret>[^/]+)/(?P<instance_inbox>[^/]+)/(?P<instance_name>[^/]+)/$',
+    url(r'^smartthings/oauth/callback/(?P<client_id>[^/]+)/(?P<client_secret>[^/]+)/(?P<instance_inbox>[^/]+)/'
+        r'(?P<instance_name>[^/]+)/(?P<sender>[^/]+)/$',
         'smartthings.views.oauth_callback',
         name='Smart Things callback'),
 )
--- smartthings/views.py
+++ smartthings/views.py
@@ -8,16 +8,23 @@ def oauth_authorize(request):
     client_secret = request.GET['client_secret']
     instance_inbox = request.GET['instance_inbox']
     instance_name = request.GET['instance_name']
-    redirect_uri = 'http://%s/smartthings/oauth/callback/%s/%s/%s/%s/' % (request.get_host(), client_id, client_secret,
-                                                                          instance_inbox, instance_name)
-    return redirect('https://graph.api.smartthings.com/oauth/authorize?response_type=code&client_id=%s&scope=app&redirect_uri=%s' % (client_id, redirect_uri))
+    sender = request.GET['sender']
+    redirect_uri = 'http://%s/smartthings/oauth/callback/%s/%s/%s/%s/%s/' % (request.get_host(), client_id,
+                                                                             client_secret, instance_inbox,
+                                                                             instance_name, sender)
+    return redirect('https://graph.api.smartthings.com/oauth/authorize?response_type=code&client_id=%s&scope=app'
+                    '&redirect_uri=%s' % (client_id, redirect_uri))
 
 
-def oauth_callback(request, client_id, client_secret, instance_inbox, instance_name):
+def oauth_callback(request, client_id, client_secret, instance_inbox, instance_name, sender):
     code = request.GET['code']
     instance_url = 'https://%s.nebrios.com/interact' % instance_name
-    redirect_uri = 'http://%s/smartthings/oauth/callback/%s/%s/%s/%s/' % (request.get_host(), client_id,
-                                                                          client_secret, instance_inbox, instance_name)
-    response = requests.get('https://graph.api.smartthings.com/oauth/token?grant_type=authorization_code&client_id=%s&client_secret=%s&redirect_uri=%s&scope=app&code=%s' % (client_id, client_secret, redirect_uri, code))
-    send_mail('', 'st_access_token := %s' % response.json()['access_token'], 'tomk@bixly.com', [instance_inbox], fail_silently=False)
+    redirect_uri = 'http://%s/smartthings/oauth/callback/%s/%s/%s/%s/%s/' % (request.get_host(), client_id,
+                                                                             client_secret, instance_inbox,
+                                                                             instance_name, sender)
+    response = requests.get('https://graph.api.smartthings.com/oauth/token?grant_type=authorization_code&client_id=%s&'
+                            'client_secret=%s&redirect_uri=%s&scope=app&code=%s' % (client_id, client_secret,
+                                                                                    redirect_uri, code))
+    send_mail('', 'st_access_token := %s' % response.json()['access_token'], sender, [instance_inbox],
+              fail_silently=False)
     return redirect(instance_url)
