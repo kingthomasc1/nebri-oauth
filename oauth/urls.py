68f21ab24c61f64664c25c453165f42f9ab3560f
craktok <king.thomas.c@gmail.com>
2014-10-20 09:54:36 -0700

    added instance name to fix redirect

--- oauth/urls.py
+++ oauth/urls.py
@@ -4,7 +4,7 @@ urlpatterns = patterns('',
     url(r'^smartthings/oauth/authorize/$',
         'smartthings.views.oauth_authorize',
         name='Smart Things authorize'),
-    url(r'^smartthings/oauth/callback/(?P<client_id>[^/]+)/(?P<client_secret>[^/]+)/(?P<instance_inbox>[^/]+)/$',
+    url(r'^smartthings/oauth/callback/(?P<client_id>[^/]+)/(?P<client_secret>[^/]+)/(?P<instance_inbox>[^/]+)/(?P<instance_name>[^/]+)/$',
         'smartthings.views.oauth_callback',
         name='Smart Things callback'),
 )
